b8e768cda39209290f60f12a448ee9b99613d582
AegisVP <vlad@pysarenko.com>
2022-06-28 14:30:56 +0200

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
