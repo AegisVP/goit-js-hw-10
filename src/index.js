8406e1207812d80a1bdae1ae3378deab0d065852
AegisVP <vlad@pysarenko.com>
2022-06-29 18:23:02 +0200

    search implemented, drawing cards, loading image added

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
-import debounce from 'lodash.debounce';
-
-const DEBOUNCE_DELAY = 300;
-
---
+++ src/js/index.js
@@ -0,0 +1,83 @@
+import '../css/styles.css';
+import '../../node_modules/notiflix/dist/notiflix-3.2.5.min.css';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries.js';
+
+const DEBOUNCE_DELAY = 300;
+
+const inputFieldRef = document.querySelector('input#search-box');
+const countryListRef = document.querySelector('ul.country-list');
+const countryInfoRef = document.querySelector('div.country-info');
+const loadingImageRef = document.querySelector('.loading-image');
+
+inputFieldRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+countryListRef.addEventListener('click', onClickLink);
+
+function onSearch(e) {
+  const searchName = e.target.value.trim();
+  showLoadingImage(true);
+  if (searchName) fetchCountries(searchName).then(countries => {
+    drawCountries(countries);
+    showLoadingImage(false);
+  });
+}
+
+function showLoadingImage (show = true) {
+  if (show) {
+    loadingImageRef.classList.remove('is-hidden');
+    inputFieldRef.disabled = true;
+    return;
+  }
+  
+  loadingImageRef.classList.add('is-hidden');
+  inputFieldRef.disabled = false;
+  inputFieldRef.focus();
+}
+
+function drawCountries(countries) {
+  countryInfoRef.innerHTML = '';
+  countryListRef.innerHTML = '';
+
+  if (countries.length > 10) {
+    Notify.info('Too many results. Please be more specific');
+  } else if (countries.length > 1) {
+    countryListRef.innerHTML = markupCountryList(countries);
+  } else if (countries.length === 1) {
+    countryInfoRef.innerHTML = markupCountryInfo(countries[0]);
+  } else {
+    Notify.failure('Nothing found with that name :(');
+  }
+}
+
+function markupCountryList(countries) {
+  console.log(countries);
+  return countries
+    .map(
+      country => `
+      <li class="country-list__item">
+        <a data-href="${country.name.official}" class="country-list__link" href="#">
+          <img  data-href="${country.name.official}"class="country-list__image" width="60" src="${country.flags.svg}" alt="${country.name.official}">
+          <p data-href="${country.name.official}" class="country-list__name">${country.name.official}</p>
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+function markupCountryInfo({ languages, flags, name, capital, population }) {
+  return `
+    <img class="country-info__image" width="90" src="${flags.svg}" alt="${name.official}">
+    <p class="country-info__text"><span class="country-info__label">Capital:</span> ${capital.join(', ')}</p>
+    <p class="country-info__text"><span class="country-info__label">Population:</span> ${population}</p>
+    <p class="country-info__text"><span class="country-info__label">Languages:</span> ${Object.values(languages).join(', ')}</p>`;
+}
+
+function onClickLink(e) {
+  e.preventDefault();
+
+  inputFieldRef.value = e.target.dataset.href.trim();
+  console.log(e.target);
+  onSearch({'target':inputFieldRef});
+}
